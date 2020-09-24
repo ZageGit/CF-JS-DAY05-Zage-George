dc65963bd19dc1bae66b1eeb75e56453134396c5
George Ciocan <george.ciocan21@gmail.com>
2020-09-24 13:11:26 +0200

    test

--- intermediate.js
+++ intermediate.js
@@ -1,1 +1,12 @@
-console.log(employ);
+console.log(employees);
+
+let heading = document.getElementById("heading").children;
+
+for (let i = 0; i < employees.length; i++) {
+  let keys = Object.keys(employees[i]);
+
+  for (let i = 0; i < heading.length; i++) {
+    heading[i].textContent = "test";
+    console.log(heading[i]);
+  }
+}
