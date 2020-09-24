d1ad15056085821a1d0a2235b6a9bedef90370af
Johannes Zarre <zarre.johannes@gmail.com>
2020-09-24 11:06:02 +0200

    finish basic

---
+++ index.js
@@ -0,0 +1,10 @@
+let bun = JSON.parse(sandwiches)
+let potato = JSON.parse(fries)
+
+console.log(`My favorite sandwich is a ${bun.sandwich} which has approximately ${bun.calories} calories,
+along with it I enjoy eating Large ${potato.fries_size} which have about ${potato.calories} calories.`)
+
+
+
+
+
