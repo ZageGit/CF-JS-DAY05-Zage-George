f40e9754a407d195fff590543e7d28fc1a49c906
George Ciocan <george.ciocan21@gmail.com>
2020-09-24 14:23:00 +0200

    test

--- intermediate.js
+++ intermediate.js
@@ -9,3 +9,18 @@ for (let i = 0; i < heading.length; i++) {
 }
 
 console.log(heading);
+
+for (let i = 0; i < employees.length; i++) {
+  let row = document.createElement("tr");
+  console.log(employees[i]);
+  for (let j = 0; j <= keys.length; j++) {
+    let column = document.createElement("td");
+    console.log(employees[i]);
+    console.log(keys[j]);
+    column.innerText = employees[i].keys[j];
+    row.appendChild(column);
+  }
+  console.log(row);
+  /*   let uniqueId = document.createElement("td")
+  uniqueId.innerText = employees[i].keys[i] */
+}
