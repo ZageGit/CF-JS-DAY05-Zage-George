ea6e701443b384dadd1d1a819aab29d47200d8b2
George Ciocan <george.ciocan21@gmail.com>
2020-09-24 13:37:05 +0200

    solves heading

--- intermediate.js
+++ intermediate.js
@@ -2,11 +2,10 @@ console.log(employees);
 
 let heading = document.getElementById("heading").children;
 
-for (let i = 0; i < employees.length; i++) {
-  let keys = Object.keys(employees[i]);
+let keys = Object.keys(employees[0]);
 
-  for (let i = 0; i < heading.length; i++) {
-    heading[i].textContent = "test";
-    console.log(heading[i]);
-  }
+for (let i = 0; i < heading.length; i++) {
+  heading[i].textContent = keys[i];
 }
+
+console.log(heading);
