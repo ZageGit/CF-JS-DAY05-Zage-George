9e45f27402a4cf17eaea1952b2d7db016c8f9a4b
George Ciocan <george.ciocan21@gmail.com>
2020-09-24 14:57:48 +0200

    final2

--- intermediate.js
+++ intermediate.js
@@ -3,12 +3,10 @@ console.log(employees);
 let heading = document.getElementById("heading").children;
 let keys = Object.keys(employees[0]);
 let table = document.getElementsByTagName("tbody")[0];
-console.log(table);
+
 for (let i = 0; i < heading.length; i++) {
   heading[i].textContent = keys[i];
 }
-
-console.log(heading);
 
 for (let i = 0; i < employees.length; i++) {
   let row = document.createElement("tr");
