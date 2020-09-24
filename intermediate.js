76438bc7ab502ba3863f7b2a87823ba76ababc03
George Ciocan <george.ciocan21@gmail.com>
2020-09-24 14:57:04 +0200

    final

--- intermediate.js
+++ intermediate.js
@@ -1,9 +1,9 @@
 console.log(employees);
 
 let heading = document.getElementById("heading").children;
-
 let keys = Object.keys(employees[0]);
-
+let table = document.getElementsByTagName("tbody")[0];
+console.log(table);
 for (let i = 0; i < heading.length; i++) {
   heading[i].textContent = keys[i];
 }
@@ -12,15 +12,12 @@ console.log(heading);
 
 for (let i = 0; i < employees.length; i++) {
   let row = document.createElement("tr");
-  console.log(employees[i]);
-  for (let j = 0; j <= keys.length; j++) {
+  row.classList.add("content");
+  for (let j = 0; j < keys.length; j++) {
     let column = document.createElement("td");
-    console.log(employees[i]);
-    console.log(keys[j]);
-    column.innerText = employees[i].keys[j];
+    column.innerText = employees[i][keys[j]];
     row.appendChild(column);
   }
   console.log(row);
-  /*   let uniqueId = document.createElement("td")
-  uniqueId.innerText = employees[i].keys[i] */
+  table.appendChild(row);
 }
